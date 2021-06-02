ce0f662d907ad5dc0d66775ae68072033af224c0
Hayley Gayfer <z5312096@ad.unsw.edu.au>
2021-06-02 16:33:37 +1000

    average

--- src/average/Average.java
+++ src/average/Average.java
@@ -9,11 +9,19 @@ public class Average {
      */
     public float computeAverage(int[] nums) {
         float result = 0;
-        // Add your code
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            result += nums[i];
+        }
+        return result / nums.length;
     }
 
     public static void main(String[] args) {
-        // Add your code
+        int[] nums = {1, 2, 3, 4, 5};
+
+        Average NewAverage = new Average();
+
+        float result = NewAverage.computeAverage(nums);
+
+        System.out.println(result);
     }
 }
